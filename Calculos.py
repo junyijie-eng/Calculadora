997fbd484d3664979d903a90ccb7a09e565fcca3
sergio.rueda <sergio.rueda@estudiantat.upc.edu>
2026-03-02 18:16:43 +0100

    inicio2

--- Calculos.py
+++ Calculos.py
@@ -1,21 +1,12 @@
 import doctest
 
-
-
->>> division(3,0)
-ZeroDivisionError: division by zero
->>> division(8,4)
-2
->>> raiz(25,2)
-5
-
 class Calculos:
 
     def suma(self,a, b):
         """
-        >>> suma(1,2)
+        >>> calculos(1,2)
         3
-        >>> suma(3,4)
+        >>> calculos(3,4)
         6
 
         :param a:
@@ -24,29 +15,37 @@ class Calculos:
         """
         return a + b
     def resta(self,a, b):
-        return a - b
         """
-        >>> resta(4,3)
+        >>> calculos(4,3)
         1
-        >>> resta(9,4)
+        >>> calculos(9,4)
         5
-        :param a: 
-        :param b: 
-        :return: 
+        :param a:
+        :param b:
+        :return:
         """
+        return a - b
+
     def multiplicacion(self,a, b):
-        return a * b
         """
         >>> multiplicacion(2,3)
         6
         >>> multiplicacion(3,4)
         12
-        
-        :param a: 
-        :param b: 
-        :return: 
+
+        :param a:
+        :param b:
+        :return:
         """
-    def division(self,a, b:
+        return a * b
+
+    def division(self,a, b):
+        """
+        >>> division(3,0)
+        ZeroDivisionError: division by zero
+        >>> division(8,4)
+        2
+        """
         try:
             return a / b
         except ZeroDivisionError:
