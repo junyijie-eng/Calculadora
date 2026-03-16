605a08e26afa6d46c6f395f6ca087bc5e4cc0724
S3rgioR <sergio.rueda@estudiantat.upc.edu>
2026-03-16 17:24:01 +0100

    PantallaCalculador

--- PantallaCalculador.py
+++ PantallaCalculador.py
@@ -1,13 +1,13 @@
 # PantallaCalculadora.py
 if __name__ == "__main__":
-from model import Model
-from view import View
-from presenter import Presenter
-app = QtWidgets.QApplication(sys.argv)
-# Instanciamos los objetos de forma independiente
-modelo = Model()
-vista = View()
-# El Presenter los une mediante AGREGACIÓN
-presentador = Presenter(vista, modelo)
-vista.show()
-sys.exit(app.exec_())
+    from model import Model
+    from view import View
+    from presenter import Presenter
+    app = QtWidgets.QApplication(sys.argv)
+    # Instanciamos los objetos de forma independiente
+    modelo = Model()
+    vista = View()
+    # El Presenter los une mediante AGREGACIÓN
+    presentador = Presenter(vista, modelo)
+    vista.show()
+    sys.exit(app.exec_())
